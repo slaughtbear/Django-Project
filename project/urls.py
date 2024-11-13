dac5b554aa6b7967c1dcf10af580099abc434dab
slaughtbear <ivanaguirre368@gmail.com>
2024-11-13 13:35:17 -0600

    Add: Rutas para la app projects

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth_app.urls'))
+    path('auth/', include('auth_app.urls')),
+    path('projects/', include('projects.urls')),
 ]
