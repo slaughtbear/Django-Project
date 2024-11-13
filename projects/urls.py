cf1a73d01ea3c771d7fc057309786d98abbe05af
slaughtbear <ivanaguirre368@gmail.com>
2024-11-13 15:34:02 -0600

    Add: Url para ver los detalles de los proyectos

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('create/', views.create_project, name='create_project'),
+    path('<int:id>/', views.project_detail, name='project_detail')
 ]
