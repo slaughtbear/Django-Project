9eca60e42568d4f7399d93d5a066c3d3ebadb7a8
slaughtbear <ivanaguirre368@gmail.com>
2024-11-13 15:55:01 -0600

    Add: Url para completar una tarea

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('create/', views.create_project, name='create_project'),
-    path('<int:id>/', views.project_detail, name='project_detail')
+    path('<int:id>/', views.project_detail, name='project_detail'),
+    path('<int:id>/complete', views.complete_project, name='complete_project'),
 ]
