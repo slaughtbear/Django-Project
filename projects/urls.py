7488d1318a27909a3f7f86ce1cea71c8c3934684
slaughtbear <ivanaguirre368@gmail.com>
2024-11-14 12:30:13 -0600

    Add: Urls para proyectos terminados y para eliminar proyectos

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create/', views.create_project, name='create_project'),
     path('<int:id>/', views.project_detail, name='project_detail'),
     path('<int:id>/complete', views.complete_project, name='complete_project'),
-    
+    path('completed/', views.projects_completed, name='projects_completed'),
+    path('<int:id>/delete', views.delete_project, name='delete_project')
 ]
