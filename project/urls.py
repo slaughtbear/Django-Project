41ecb61be825a17e2abb5515c047e19ce9f63df3
slaughtbear <ivanaguirre368@gmail.com>
2024-11-14 11:44:07 -0600

    Add: Urls de la app landing

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('landing.urls')),
     path('auth/', include('auth_app.urls')),
     path('projects/', include('projects.urls')),
 ]
